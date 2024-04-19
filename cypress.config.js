fa6b7c3ce85787d4095808dbae677e83593fd6a7
shraddhashetty212 <shraddhashetty212@gmail.com>
2024-04-19 21:58:47 +0200

    third commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: 'cypress/integration/tasks/*.js',
+    projectId: "g72ii3",
     env: {
       url : "https://computer-database.gatling.io/computers"
     }
